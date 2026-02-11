bbbaa50be127161b5a31f3c3252a6c98a058a51a
kushal121231 <kushalnaganaboina3@gmail.com>
2026-02-11 22:36:21 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -52,5 +53,6 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = dataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
-    
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrianer = ModelTrainer()
+    modeltrianer.initiate_model_trainer(train_arr, test_arr)
